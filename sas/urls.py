5dd7d7fabe4629f10d5cdd118e55519335bd0318
Sinderella <rawsec@gmail.com>
2014-10-10 21:46:38 +0100

    Test

--- sas/urls.py
+++ sas/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', 'web.views.current_datetime')
+    url(r'^$', 'web.views.current_datetime')
 )
