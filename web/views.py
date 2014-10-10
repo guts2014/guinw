1d3c5bf7a3d5a9ff94dc1720c6588d9d344c4e72
Sinderella <rawsec@gmail.com>
2014-10-10 21:43:28 +0100

    Test

--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
