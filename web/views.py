6f55302335a249787245db548e80e088497a4f9d
Sinderella <rawsec@gmail.com>
2014-10-11 09:00:35 +0100

    add comment

--- web/views.py
+++ web/views.py
@@ -8,7 +8,11 @@ def index(request):
 def search(request):
     if request.method == 'GET':
         return render(request, 'search.html')
+    query = request.POST.get('query', '')
+    # collect data from CSV and list it
+    return render(request, 'search.html', {'query': query})
 
 
 def detail(request, eid):
+    # gathering information from wikipedia and get images from sources
     return render(request, 'detail.html', {'eid': eid})
