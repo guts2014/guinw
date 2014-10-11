735e48cf2075cfbdc1bd6dd4969f25aee2c6bc98
Sinderella <rawsec@gmail.com>
2014-10-11 14:18:05 +0100

    views.py: control per page viewing

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
-import mmap
+from mmap import mmap
+
 import wikipedia
 from django.shortcuts import render
+
 from sas.settings import BASE_DIR
 
 
@@ -8,14 +10,14 @@ def index(request):
     return render(request, 'index.html')
 
 
-def search(request, page=0):
+def search(request, line=10, page=0):
     query = request.GET.get('query', '')
     # collect data from CSV and list it
     # read CSV with filter
     data = list()
     with open((BASE_DIR + '/media/globalterrorismdb_0814dist.csv'), 'r+b') as f:
         # memory-mapInput the file, size 0 means whole file
-        map_input = mmap.mmap(f.fileno(), 0)
+        map_input = mmap(f.fileno(), 0)
         # read content via standard file methods
         for s in iter(map_input.readline, ""):
             s = s.decode('latin-1')
@@ -23,8 +25,10 @@ def search(request, page=0):
                 data.append(s.split(','))
         map_input.close()
     f.close()
-    total = len(data)
-    data = data[-9:]
+
+    data = data[::-1]
+    data = data[line * page:line * page + line]
+
     # return data
     return render(request, 'search.html', {'query': query, 'data': data})
 
