3bf6f6ea6a12e1894306aa2f961420f718debe7e
SmAaMyA <smaamya@windowslive.com>
2014-10-12 01:28:28 +0100

    fix encoding problem

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from csv import reader
 import re
-# from mmap import mmap
+
 from django.shortcuts import render
 import simplejson
 import urllib2
@@ -39,23 +39,11 @@ def getdata(query, line, page):
                 data.append(row)
     csv_file.close()
 
-    # with open((BASE_DIR + '/media/globalterrorismdb_0814dist.csv'), 'r+b') as f:
-    #     # memory-mapInput the file, size 0 means whole file
-    #     map_input = mmap(f.fileno(), 0)
-    #     # read content via standard file methods
-    #     for s in iter(map_input.readline, ''):
-    #         s = s.decode('latin-1')
-    #         if s.find(query) != -1:
-    #             data.append(s.split(','))
-    #     map_input.close()
-    # f.close()
-
     # sort by date, most recent event come first
     data.reverse()
     # partitioning elements to show in one page
     data = data[line * page:line * page + line]
 
-    # return data
     return data
 
 
@@ -73,17 +61,21 @@ def detail(request, eid, altid=0):
     data = getdata(eid, 1, 0)
 
     # grab keywords from data
-    column = (1, 2, 3, 8, 12)
+    column = (1, 2, 3, 8, 12, 29)
     keywords = ''
     for col in column:
-        keywords += data[0][col] + ' '
+        if col == 2:
+            keywords += str(MONTH[int(data[0][col])]) + ' '
+        else:
+            keywords += data[0][col] + ' '
 
     titles = wikipedia.search(keywords)
+    for i in xrange(len(titles)):
+        titles[i] = titles[i].encode('utf-8')
 
     page = wikipedia.page(titles[altid])
 
     # get images from google using ajax from googleapis
-
     searchTerm = titles[altid]
 
     img_urls = []
@@ -102,10 +94,10 @@ def detail(request, eid, altid=0):
     content = page.content
     content = re.sub('[\=]+([A-Za-z\ ])+[\=]+', __replace, content)
 
-    relevant = []
+    relevant = list()
     i = 0
     for topic in titles:
-        tmp = []
+        tmp = list()
         tmp.append(topic)
         tmp.append('/' + str(eid) + '/' + str(i) + '/')
         if i == altid:
